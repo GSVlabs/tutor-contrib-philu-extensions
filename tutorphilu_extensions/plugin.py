8c81a5478e5852297a7d4387df51650803c93e62
Dima Alipov <dimaalipov@192.168.1.101>
2024-06-26 12:11:03 +0300

    fix: [PhU-328] Missed favicon and platform name on diff pages
    YT:
    - https://youtrack.raccoongang.com/issue/PhU-328

--- tutorphilu_extensions/plugin.py
+++ tutorphilu_extensions/plugin.py
@@ -167,11 +167,11 @@ hooks.Filters.ENV_PATCHES.add_items(
     [
         (
             "mfe-dockerfile-pre-npm-build-discussions",
-            "ENV USE_LR_MFE='true'"
+            "ENV FAVICON_URL='{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}/static/rg-theme/images/favicon-mfe.ico'"
         ),
         (
             "mfe-dockerfile-pre-npm-build-learner-record",
-            "ENV FAVICON_URL='{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}/static/rg-theme/images/favicon-mfe.ico'"
+            "ENV USE_LR_MFE='true'"
         ),
         (
             "mfe-dockerfile-post-npm-instal-authn",
